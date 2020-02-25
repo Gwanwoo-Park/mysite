bbb30c4916f2894c45f27dab2f5dc913dbf9f058
R35 <R35@DESKTOP-EA4BM1D>
2020-02-25 16:49:27 +0900

    UserRepository Mybatis 적용

--- mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -24,144 +26,23 @@ public class UserRepository {
 	private SqlSession sqlSession;
 	
 	public int insert(UserVo vo) {
-		int count = 0;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			conn = dataSource.getConnection();
-
-			String sql = " insert" + 
-						 "   into user" + 
-						 " values(null, ?, ?, ?, ?, now())";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-
-			count = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			throw new UserRepositoryException("error : " + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return count;
+		return sqlSession.insert("user.insert", vo);
 	}
 
 	public UserVo findByEmailAndPassword(UserVo vo) {
-		UserVo userVo = null;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn = dataSource.getConnection();
-
-			String sql = 
-					"select no, name" + 
-					"  from user" + 
-					" where email = ?" +
-					"   and password = ?";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, vo.getEmail());
-			pstmt.setString(2, vo.getPassword());
-			rs = pstmt.executeQuery();
-
-			if(rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-				
-				userVo = new UserVo();
-				userVo.setNo(no);
-				userVo.setName(name);
-			}
-
-		} catch (SQLException e) {
-			throw new UserRepositoryException("error : " + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return userVo;
+		return sqlSession.selectOne("user.findByEmailAndPassword", vo);
+	}
+	
+	public UserVo findByEmailAndPassword(String email, String password) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("e", email);
+		map.put("p", password);
+		
+		return sqlSession.selectOne("user.findByEmailAndPassword2", map);
 	}
 
 	public UserVo find(Long no) {
-		UserVo userVo = null;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn = dataSource.getConnection();
-
-			String sql = 
-					"select name, email, gender" + 
-					"  from user" + 
-					" where no = ?";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setLong(1, no);
-			rs = pstmt.executeQuery();
-
-			if(rs.next()) {
-				String name = rs.getString(1);
-				String email = rs.getString(2);
-				String gender = rs.getString(3);
-				
-				userVo = new UserVo();
-				userVo.setNo(no);
-				userVo.setName(name);
-				userVo.setEmail(email);
-				userVo.setGender(gender);
-			}
-
-		} catch (SQLException e) {
-			throw new UserRepositoryException("error : " + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return userVo;
+		return sqlSession.selectOne("user.find", no);
 	}
 
 	public void update(UserVo vo) {
